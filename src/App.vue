fdb3feeaff3e6590d25ba9580e811778385b9b12
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-07-03 16:48:38 -0500

    Bulma & Design

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,17 @@
 <template>
-  <RouterLink to="/">Notes</RouterLink> | 
-  <RouterLink to="/stats">Stats</RouterLink>
-  <RouterView />
+  <NavBar />
+
+  <div class="container is-max-desktop px-2 py-4">
+    <RouterView />
+  </div>  
 </template>
 
+<script setup>
+/* imports */
+import NavBar from '@/components/Layout/NavBar.vue'
 
+</script>
+
+<style>
+@import 'bulma/css/bulma.min.css';
+</style>
