834fe683911e3ba59e230638ebb77b73d5944556
Laura <41702570+SuitcaseCoder@users.noreply.github.com>
2022-08-01 10:05:58 -0500

    directives, watchers, & composables

--- src/components/Layout/NavBar.vue
+++ src/components/Layout/NavBar.vue
@@ -17,6 +17,7 @@
         aria-label="menu"
         data-target="navbarBasicExample"
         role="button"
+        ref="navbarBurgerRef"
         >
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
@@ -28,6 +29,7 @@
     id="navbarBasicExample"
     class="navbar-menu"
     :class="{ 'is-active' : showMobileNav }"
+    ref="navbarMenuRef"
     >
     <div class="navbar-end">
         <RouterLink
@@ -56,12 +58,23 @@
 <script setup>
 /* imports */
 import { ref } from 'vue'
+// coming from vue use
+import { onClickOutside } from '@vueuse/core'
 
 
 /* mobile nav */
 // determines whether or not navbar is being shown or not
 const showMobileNav = ref(false)
 
+/* click outside to close */
+const navbarMenuRef = ref(null)
+const navbarBurgerRef = ref(null)
+
+ onClickOutside(navbarMenuRef, () => {
+  showMobileNav.value = false
+ }, {
+  ignore: [navbarBurgerRef]
+ })
 
 </script>
 
